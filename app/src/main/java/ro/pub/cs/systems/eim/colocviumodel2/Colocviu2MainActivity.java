225b38174b4159102c1f301f8b7203005f4e3de6
Isar Ioana <ioanaisar0@gmail.com>
2023-04-06 05:07:58 +0300

    Initial commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviumodel2/Colocviu2MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.colocviumodel2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colocviu2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu2_main);
+    }
+}
